3741dd7747a27edf01cad98a4b468379175c83cb
I had a bad idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-06-29 11:45:13 +0000

    Add logic for flooding with comments and likes

--- static/js/pitch.js
+++ static/js/pitch.js
@@ -53,3 +53,35 @@ voreBtn.addEventListener("click", async (e) => {
         alert("Unable to connect to the server.");
     }
 });
+
+const comments = [];
+const authors = [];
+const dates = [];
+
+const voteBtn = document.querySelector(".vote-btn");
+var votes = parseInt(voreBtn.textContent.split(" ")[1]);
+
+const totalLikes = 150;
+const totalComments = 50; // should always be less then likes 
+const waitMsBetweenComments = 10;
+const likesPerComment = totalLikes / totalComments;
+
+async function flood_with_likes_and_comments() {
+    for (let i = 0; i < totalComments; i++) {
+        await new Promise(resolve => setTimeout(resolve, waitMsBetweenComments)); // wait
+        const author = authors[Math.floor(Math.random() * authors.length)];
+        const comment = comments[Math.floor(Math.random() * comments.length)];
+        const date = dates[Math.floor(Math.random() * dates.length)];
+
+        const li = document.createElement("li");
+        li.className = "comment"
+
+        li.innerHTML = `
+        <p class="comment-text">${comment}</p>
+        <p class"comment-meta">By ${author } on ${date}</p>
+        `;
+    }
+}
+
+// After showing the pitch, we want to flood it (for the ad)
+flood_with_likes_and_comments();
