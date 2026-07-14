31907f409cb64ac879d7e0004ab3c130da2a9a67
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-07-14 18:34:39 +0200

    Log the public directory

--- api/index.go
+++ api/index.go
@@ -4,8 +4,9 @@ package handler
 import (
 	"net/http"
 	"fmt"
-	"os"
 	"sync"
+	"log"
+	"os"
 
 	"pitch.ideas/pkg/server"
 )
@@ -20,6 +21,18 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Init called")
 		router = server.NewRouter()
 	})
+
+	wd, _ := os.Getwd()
+	log.Println("Working dir:", wd)
+
+	entries, err := os.ReadDir("public")
+	if err != nil {
+		log.Println(err)
+	} else {
+		for _, e := range entries {
+			log.Println(e.Name())
+		}
+	}
 
     fmt.Println("Handler called")
 	router.ServeHTTP(w, r)
